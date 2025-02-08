ff6146e40297064430cb767de02859f07417f6d3
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-02-08 21:08:18 +0700

    wellness screen

---
+++ app/src/main/java/com/id/ac/umn/muhammad_fabiansyah_abubakar_68933/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,9 @@
+package com.id.ac.umn.muhammad_fabiansyah_abubakar_68933.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
