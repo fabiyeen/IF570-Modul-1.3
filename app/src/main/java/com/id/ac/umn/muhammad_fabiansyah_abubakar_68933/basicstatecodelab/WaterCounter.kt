b38d97949521e80ec43114c78d7178adf53fb61b
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-02-08 21:13:38 +0700

    remember

--- app/src/main/java/com/id/ac/umn/muhammad_fabiansyah_abubakar_68933/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/id/ac/umn/muhammad_fabiansyah_abubakar_68933/basicstatecodelab/WaterCounter.kt
@@ -9,13 +9,12 @@ import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        // Changes to count are now tracked by Compose
-        val count: MutableState<Int> = mutableStateOf(0)
-
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
         Text("You've had ${count.value} glasses.")
         Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
