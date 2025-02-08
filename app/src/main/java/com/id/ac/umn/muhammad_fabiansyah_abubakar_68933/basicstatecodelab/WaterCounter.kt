d7b46c172a84b7aad2c749ec5651193a2f4b2bda
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-02-08 21:20:13 +0700

    backagain

--- app/src/main/java/com/id/ac/umn/muhammad_fabiansyah_abubakar_68933/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/id/ac/umn/muhammad_fabiansyah_abubakar_68933/basicstatecodelab/WaterCounter.kt
@@ -14,31 +14,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Row
 
-
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text("You've had $count glasses.")
         }
-
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(onClick = { count++ }, enabled = count < 10) {
-                Text("Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                Modifier.padding(start = 8.dp)) {
-                Text("Clear water count")
-            }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
