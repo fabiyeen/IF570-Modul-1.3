1aa0206bf83433af5143ffe144d4cf46277e3037
fabiyeen <163744056+fabiyeen@users.noreply.github.com>
2025-02-08 21:22:03 +0700

    stateless

--- app/src/main/java/com/id/ac/umn/muhammad_fabiansyah_abubakar_68933/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/id/ac/umn/muhammad_fabiansyah_abubakar_68933/basicstatecodelab/WaterCounter.kt
@@ -16,13 +16,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
